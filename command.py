a854c4f041cafbd3963db590515c6d4a46d4c96f
Mikhail Zakharov <mzakharo@gmail.com>
2021-03-04 19:57:05 -0500

    tank controls

--- command.py
+++ command.py
@@ -1,47 +1,58 @@
 import socket
 import msgpack
 import time
+import pygame
 import sys
-from xbox360controller import Xbox360Controller
 
-if len(sys.argv) > 1:
-    UDP_IP = "192.168.50.175"
-else:
-    UDP_IP = '127.0.0.1'
+#if len(sys.argv) > 1:
+#    #UDP_IP = "192.168.50.175"
+#else:
+#    UDP_IP = '127.0.0.1'
+
+UDP_IP = "192.168.50.243"
 UDP_PORT = 5005
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+
+pygame.init()
+pygame.joystick.init()
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
+print(joystick.get_name())
+print(joystick.get_numaxes())
+print(joystick.get_numbuttons())
+
 msg = dict(
-        drive_value = 0.0,
-        turn_value = 0.0, 
-        drive_direction='forward', 
-        turn_direction='left',
+        drivel_value = 0.0,
+        driver_value = 0.0, 
+        drivel_direction='forward', 
+        driver_direction='forward',
         )
 s.sendto(msgpack.dumps(msg), (UDP_IP, UDP_PORT))
 
-controller = Xbox360Controller(0, axis_threshold=0.2)
+THL = 1
+THR = 4
 
-def get_dv():
-    return controller.axis_r.y
-def get_tv():
-    return controller.axis_l.x
-def get_reverse():
-    return controller.button_b.is_pressed
 
 #handle joystick reset -> wait for first button from user
-while get_dv() < 0.5:
-    time.sleep(0.1)
+#while joystick.get_axis(TH) < 0.5:
+#    pygame.event.pump()
+#    time.sleep(0.1)
 
 while True:
     time.sleep(1/20)
-    dv = (get_dv() + 1) / 2
-    msg['drive_value'] = 0 if dv < 0.1 else dv
-    msg['drive_direction'] = 'forward' if get_reverse() == 1 else 'b'
-    msg['turn_direction'] = 'right' if get_tv() < 0 else 'left'
-    tv = abs(get_tv())
-    msg['turn_value'] = 0 if tv < 0.1 else tv
+    pygame.event.pump()
+
+    dl = abs(joystick.get_axis(THL))
+    msg['drivel_value'] = 0 if dl < 0.1 else dl
+    msg['drivel_direction'] = 'forward' if joystick.get_axis(THL) > 0 else 'b'
+
+    dr = abs(joystick.get_axis(THR))
+    msg['driver_value'] = 0 if dr < 0.1 else dr
+    msg['driver_direction'] = 'forward' if joystick.get_axis(THR) > 0 else 'b'
+
     s.sendto(msgpack.dumps(msg), (UDP_IP, UDP_PORT))
 
-    #print(joystick.get_axis(0), joystick.get_axis(1), joystick.get_axis(2), joystick.get_axis(3), joystick.get_axis(4), joystick.get_axis(5))
+    print(joystick.get_axis(0), joystick.get_axis(1), joystick.get_axis(2), joystick.get_axis(3), joystick.get_axis(4), joystick.get_axis(5))
 
     #for i in range(joystick.get_numbuttons()):
     #    print(joystick.get_button(i), end =' ')
